bed8b58c023567c8e70537170fb56e186d6d8ee4
D.J. Saderholm <dj@saderholm.us>
2025-05-18 20:31:13 -0600

    More issues

--- webapp/core_app/timezone.py
+++ webapp/core_app/timezone.py
@@ -6,25 +6,38 @@ def get_timezone():
     """Get timezone from environment variable or fall back to UTC"""
     return os.environ.get('TIMEZONE', 'UTC')
 
-def localize_timestamp(timestamp_str):
-    """Convert UTC timestamp to local time"""
-    if not timestamp_str:
+def localize_timestamp(timestamp):
+    """Convert UTC timestamp to local time.
+    
+    Works with both string timestamps (from SQLite) and 
+    datetime objects (from PostgreSQL).
+    """
+    if not timestamp:
         return None
     
-    # Parse the timestamp (assuming it's in UTC)
-    try:
-        # First try ISO format with microseconds
-        dt = datetime.fromisoformat(timestamp_str)
-    except ValueError:
+    # Check if we already have a datetime object (from PostgreSQL)
+    if isinstance(timestamp, datetime):
+        dt = timestamp
+    else:
+        # Parse the timestamp string (from SQLite)
         try:
-            # Then try parsing with microseconds
-            dt = datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S.%f')
+            # First try ISO format
+            dt = datetime.fromisoformat(timestamp)
         except ValueError:
-            # If that fails, try without microseconds
-            dt = datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S')
+            try:
+                # Then try parsing with microseconds
+                dt = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
+            except ValueError:
+                # If that fails, try without microseconds
+                dt = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
     
-    # Set UTC timezone
-    utc_dt = pytz.UTC.localize(dt)
+    # Handle timezone - assume naive datetime is in UTC
+    if dt.tzinfo is None:
+        # Set UTC timezone
+        utc_dt = pytz.UTC.localize(dt)
+    else:
+        # Already has timezone info
+        utc_dt = dt
     
     # Convert to local timezone
     local_tz = pytz.timezone(get_timezone())
